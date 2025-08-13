700b0621f48f8a4a2c23311e116d4f969a432de2
IgnM4 <ig.mino@duocuc.cl>
2025-08-12 23:48:07 -0400

    fix: configure vitest and update tests

--- web/src/test/setup.ts
+++ web/src/test/setup.ts
@@ -1,2 +1,5 @@
 import 'whatwg-fetch';
-import '@testing-library/jest-dom';
+import * as matchers from '@testing-library/jest-dom/matchers';
+import { expect } from 'vitest';
+
+expect.extend(matchers);
